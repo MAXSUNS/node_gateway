3d3b05a8b939233cfb2b2d5079d8f61711f4a6b8
MAXSUNS <sundai.cool@hotmail.com>
2018-01-16 17:49:57 +0800

    build wechat

--- controllers/wechatController.js
+++ controllers/wechatController.js
@@ -77,26 +77,7 @@ router.get('/', function (ctx, next) {
     }
 });
 
-router.post('/', function (ctx, next) {
-    // const token = config.wechat.token, // 自定义，与公众号设置的一致
-    //     signature = ctx.query.signature,
-    //     timestamp = ctx.query.timestamp,
-    //     nonce = ctx.query.nonce
-    //
-    // // 字典排序
-    // const arr = [token, timestamp, nonce].sort()
-    //
-    // const sha1 = crypto.createHash('sha1')
-    // sha1.update(arr.join(''))
-    // const result = sha1.digest('hex')
-    //
-    // if (result === signature) {
-    //     ctx.body = ctx.query.echostr
-    // } else {
-    //     ctx.body = { code: -1, msg: "fail"}
-    // }
-    this.handler;
-});
+router.post('/', handler);
 
 router.post('/js/config', function*() {
 
