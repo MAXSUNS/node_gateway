df255d43a7a4597877a6eabc5884ecad8f62c7da
MAXSUNS <sundai.cool@hotmail.com>
2018-01-16 18:25:14 +0800

    build wechat

--- controllers/wechatController.js
+++ controllers/wechatController.js
@@ -15,44 +15,10 @@ const crypto = require('crypto')
 const QR_CODE_EXPIRED_IN = 1800;
 
 const handler = new Wechat(config.wechat).middleware(async (message, ctx) => {
-    // 微信输入信息就是这个 message
-    if (message.Content === 'diaosi') {
-        // 回复屌丝(普通回复)
-        return 'hehe';
-    } else if (message.Content === 'text') {
-        //你也可以这样回复text类型的信息
-        return {
-            content: 'text object',
-            type: 'text'
-        };
-    } else if (message.Content === 'hehe') {
-        // 回复一段音乐
-        return {
-            type: "music",
-            content: {
-                title: "来段音乐吧",
-                description: "一无所有",
-                musicUrl: "http://mp3.com/xx.mp3",
-                hqMusicUrl: "http://mp3.com/xx.mp3"
-            }
-        };
-    } else if (message.Content === 'kf') {
-        // 转发到客服接口
-        return {
-            type: "customerService",
-            kfAccount: "test1@test"
-        };
-    } else {
-        // 回复高富帅(图文回复)
-        return [
-            {
-                title: '你来我家接我吧',
-                description: '这是女神与高富帅之间的对话',
-                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-                url: 'http://nodeapi.cloudfoundry.com/'
-            }
-        ];
-    }
+    return {
+        content: 'text object'+message.Content,
+        type: 'text'
+    };
 });
 
 
@@ -78,6 +44,11 @@ router.get('/', function (ctx, next) {
 });
 
 router.post('/', handler);
+router.post('/register', async (ctx, next) => {
+    console.log(ctx.request.body);
+    let reqBody = ctx.request.body;
+    ctx.body = "234234";
+});
 
 router.post('/js/config', function*() {
 
